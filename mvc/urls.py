2f29f6e08bd8925ef46fc5c16c10dd522e0d2c00
ajian911 <zhengjian813@163.com>
2020-05-11 15:35:49 +0800

    addSite&addExam

--- mvc/urls.py
+++ mvc/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'addSite', views.addSite),
     url(r'addExam', views.addExam),
-    url(r'', views.addExamResult, name = 'add-exam-result'),
-    url(r'', views.addSiteResult, name = 'add-site-result'),
+    url(r'1', views.addExamResult, name = 'add-exam-result'),
+    url(r'2', views.addSiteResult, name = 'add-site-result'),
 ]
